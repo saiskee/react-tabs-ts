0bf915a327cadcc5086476e2688e760f0a10f9fa
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2016-11-23 23:54:33 +0100

    00 Boilerplate documentation
    Missing Testing section

---
+++ 00 Boilerplate/webpack.config.js
@@ -0,0 +1,84 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+  context: path.join(basePath, 'src'),
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  entry: [
+    './main.ts',
+    './styles.scss',
+    '../node_modules/bootstrap/dist/css/bootstrap.css'
+  ],
+  output: {
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.[id].js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(basePath, 'dist'),
+    inline: true,
+    host: '0.0.0.0',
+    port: 8080,
+    stats: 'errors-only',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader', 'sass-loader'],
+        }),
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: ['css-loader'],
+        }),
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(basePath, 'src', 'index.html'),
+      hash: true,
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles.[id].css',
+      disable: false,
+      allChunks: true,
+    }),
+  ],
+};
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # react-tabs-ts
-A simple tabs components using React and TypeScript
+This repository was created to create a simple React tabs component from scracth using Bootstrap.
+We will first set up our workspace, configure some dev-tools and plumbing and finally get down to business.
+
+This repository will use the next tools:
+- [Node.js ^7.1.0](https://nodejs.org) This will be our 
+- [TypeScript ^2.1.1](https://www.typescriptlang.org)
+- [React](https://facebook.github.io/react)
+- [Webpack](https://webpack.github.io)
+- [Karma](https://karma-runner.github.io)
+- [Mocha](https://mochajs.org)
+- [Chai](http://chaijs.com)
+
+It is grouped by folders, each with its own purpose. The structure is as follows:
+
+### 00 Boilerplate
+I this folder we'll explain how to set up  step by step the environment. We'll configure some tools like 
+webpack for bundling, transpiling TypeScript files, automatic build generation...
+ for running our tabs component tests, etc...
